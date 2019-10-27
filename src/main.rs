cc3f4c1a89645199eb8b2be0d7dcf59f314f423d
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-10-26 18:40:33 -0700

    pool testing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Write, Read};
 use futures::executor::ThreadPool;
 use std::str::from_utf8;
+use std::sync::RwLock;
+use futures::task::SpawnExt;
+use std::thread::sleep;
+use std::time::Duration;
 
 const ERROR_MSG: &[u8] = b"Error: Invalid command\r\n";
 
@@ -14,10 +18,10 @@ async fn handle_client(mut stream: TcpStream) {
         let num_bytes = stream.read(&mut buf).unwrap();
         if num_bytes <= 0 { return; }
         let s = from_utf8(&buf[0..num_bytes]).unwrap();
-        println!("Got {} bytes, msg: {}", num_bytes, s);
+//        println!("Got {} bytes, msg: {}", num_bytes, s);
 
-        s.find(' ').and_then(|len| match s[..len].as_ref() {
-            "GET" => Some(stream.write(b"got it").unwrap()),
+        s.find(' ').and_then(|len| match &s[..len] {
+            "GET" => getReq(&mut stream, &s[len..]),
             "SET" => Some(stream.write(b"set it").unwrap()),
             _ => {
                 stream.write(ERROR_MSG).unwrap();
@@ -27,13 +31,19 @@ async fn handle_client(mut stream: TcpStream) {
     };
 }
 
+fn getReq(stream: &mut TcpStream, key: &str) -> Option<usize> {
+    let val = 5;
+    sleep(Duration::from_millis(500));
+    return Some(stream.write(format!("got {}", val).as_ref()).unwrap());
+}
 
 fn main() {
-    let pool = ThreadPool::new().expect("Failed to create threadpool");
+    let pool = ThreadPool::builder().pool_size(1).create().unwrap();
+//    let pool = ThreadPool::new().expect("Failed to create threadpool");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     print!("Listening\n");
 
-// accept connections and process them serially
+    // accept connections and process them serially
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => pool.spawn_ok(handle_client(stream)),

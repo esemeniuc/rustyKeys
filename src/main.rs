a211aa5a4f9c97bc6fdad3bc3ef63e053a7b9bf9
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-10-28 13:57:06 -0700

    add async

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,55 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Read};
-use futures::executor::ThreadPool;
+use async_std::io;
+use async_std::net::{TcpListener, TcpStream};
+use async_std::prelude::*;
+use async_std::task;
+use std::io::{Error, ErrorKind};
 use std::str::from_utf8;
-use std::sync::RwLock;
-use futures::task::SpawnExt;
-use std::thread::sleep;
-use std::time::Duration;
+//use std::sync::RwLock;
 
 const ERROR_MSG: &[u8] = b"Error: Invalid command\r\n";
 
-async fn handle_client(mut stream: TcpStream) {
-    stream.write(b"Welcome\n").unwrap();
+async fn process(mut stream: TcpStream) -> io::Result<()> {
+    println!("Accepted from: {}", stream.peer_addr()?);
+
+    stream.write(b"Welcome\n").await?;
     print!("got connection!\n");
     let mut buf = [0; 1024]; //1KB
 
     loop {
-        let num_bytes = stream.read(&mut buf).unwrap();
-        if num_bytes <= 0 { return; }
+        let num_bytes = stream.read(&mut buf).await?;
+        if num_bytes <= 0 { return Err(Error::new(ErrorKind::ConnectionAborted, "no bytes")); };
         let s = from_utf8(&buf[0..num_bytes]).unwrap();
 //        println!("Got {} bytes, msg: {}", num_bytes, s);
 
-        s.find(' ').and_then(|len| match &s[..len] {
-            "GET" => getReq(&mut stream, &s[len..]),
-            "SET" => Some(stream.write(b"set it").unwrap()),
-            _ => {
-                stream.write(ERROR_MSG).unwrap();
-                None
-            }
-        });
+        match s.find(' ') {
+            Some(len) => match &s[..len] {
+                "GET" => stream.write(get_req(&s[len..]).as_ref()).await?,
+                "SET" => stream.write(b"set it").await?,
+                _ => stream.write(ERROR_MSG).await?
+            },
+            None => stream.write(ERROR_MSG).await?
+        };
     };
 }
 
-fn getReq(stream: &mut TcpStream, key: &str) -> Option<usize> {
+fn get_req(key: &str) -> String {
     let val = 5;
-    sleep(Duration::from_millis(500));
-    return Some(stream.write(format!("got {}", val).as_ref()).unwrap());
+    format!("got {}", val)
 }
 
-fn main() {
-    let pool = ThreadPool::builder().pool_size(1).create().unwrap();
-//    let pool = ThreadPool::new().expect("Failed to create threadpool");
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    print!("Listening\n");
+fn main() -> io::Result<()> {
+    task::block_on(async {
+        let listener = TcpListener::bind("127.0.0.1:8080").await?;
+        println!("Listening on {}", listener.local_addr()?);
 
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => pool.spawn_ok(handle_client(stream)),
-            Err(e) => println!("Error receiving incoming connection: {}", e)
+        let mut incoming = listener.incoming();
+
+        while let Some(stream) = incoming.next().await {
+            let stream = stream?;
+            task::spawn(async {
+                process(stream).await.unwrap();
+            });
         }
-    }
+        Ok(())
+    })
 }

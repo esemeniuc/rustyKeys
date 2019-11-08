35814e963551558369f7789641dbabd4d76d1014
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-11-07 16:09:44 -0800

    add mutex

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use async_std::prelude::*;
 use async_std::task;
 use std::io::{Error, ErrorKind};
 use std::str::from_utf8;
+use async_std::sync::{Arc, Mutex};
+
 //use std::sync::RwLock;
 const ERROR_MSG: &[u8] = b"Error: Invalid command\r\n";
 
-async fn process(mut stream: TcpStream) -> io::Result<()> {
+async fn process(mut stream: TcpStream, m: Arc<Mutex<i32>>) -> io::Result<()> {
     println!("Accepted from: {}", stream.peer_addr()?);
 
     stream.write(b"Welcome\n").await?;
@@ -18,12 +20,14 @@ async fn process(mut stream: TcpStream) -> io::Result<()> {
         let num_bytes = stream.read(&mut buf).await?;
         if num_bytes <= 0 { return Err(Error::new(ErrorKind::ConnectionAborted, "no bytes")); };
         let s = from_utf8(&buf[0..num_bytes]).unwrap();
+        let s1 = from_utf8(&buf[0..num_bytes]).unwrap();
 //        println!("Got {} bytes, msg: {}", num_bytes, s);
 
         match s.find(' ') {
             Some(len) => match &s[..len] {
-                "GET" => stream.write(get_req(&s[len..]).as_ref()).await?,
+                "GET" => stream.write(get_req(&s[len..], &m).await.as_ref()).await?,
                 "SET" => stream.write(b"set it").await?,
+//                "SET" => stream.write(set_req(&s[len..], &s1[len..],&m).await.as_ref()).await?,
                 _ => stream.write(ERROR_MSG).await?
             },
             None => stream.write(ERROR_MSG).await?
@@ -31,12 +35,20 @@ async fn process(mut stream: TcpStream) -> io::Result<()> {
     };
 }
 
-fn get_req(key: &str) -> String {
-    let val = 5;
-    format!("got {}", val)
+async fn get_req(key: &str, m: &Arc<Mutex<i32>>) -> String {
+    let mut num = m.lock().await;
+    (*num) = *num + 1;
+    format!("got {}\n", *num)
+}
+
+async fn set_req(key: &str, val: &str, m: &Arc<Mutex<i32>>) -> String {
+    let mut num = m.lock().await;
+    (*num) = *num + 1;
+    format!("set {}\n", *num)
 }
 
 fn main() -> io::Result<()> {
+    let counter = Arc::new(Mutex::new(0));
     task::block_on(async {
         let listener = TcpListener::bind("127.0.0.1:8080").await?;
         println!("Listening on {}", listener.local_addr()?);
@@ -45,8 +57,9 @@ fn main() -> io::Result<()> {
 
         while let Some(stream) = incoming.next().await {
             let stream = stream?;
+            let counter = Arc::clone(&counter);
             task::spawn(async {
-                process(stream).await.unwrap();
+                process(stream, counter).await.unwrap();
             });
         }
         Ok(())

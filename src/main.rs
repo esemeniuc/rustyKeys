a8800b3f1c66bcb94de1b56c95ba0f248cd78c33
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-11-21 20:44:00 -0800

    comply with redis spec

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,10 @@ use async_std::sync::{Arc, RwLock};
 use std::collections::HashMap;
 use commands::tokenizer::tokenize;
 
-const ERROR_MSG: &[u8] = b"Error: Invalid command\r\n";
-const NIL_MSG: &[u8] = b"$-1\r\n";
+const ERR_UNK_CMD: &[u8] = b"-ERR unknown command\r\n";
+const NIL_MSG: &str = "$-1\n";
 
 type TestRCMap<T, U> = Arc<RwLock<HashMap<T, U>>>;
-
 
 async fn process(mut stream: TcpStream, dict: TestRCMap<String, String>) -> io::Result<()> {
     println!("Accepted from: {}", stream.peer_addr()?);
@@ -25,16 +24,16 @@ async fn process(mut stream: TcpStream, dict: TestRCMap<String, String>) -> io::
         let s = from_utf8(&buf[0..num_bytes]).unwrap().trim(); //TODO handle UTF8
         if let Ok(tokens) = tokenize(s) {
             if tokens.len() == 0 {
-                stream.write(ERROR_MSG).await?;
+                stream.write(ERR_UNK_CMD).await?;
             }
             match (tokens[0].text, tokens.len()) {
                 ("GET", 3) => stream.write(get_req(tokens[2].text, &dict).await.as_ref()).await?,
                 ("SET", 5) => stream.write(set_req(tokens[2].text, tokens[4].text, &dict).await.as_ref()).await?,
                 ("DEL", 3) => stream.write(del_req(vec![tokens[2].text], &dict).await.as_ref()).await?, //TODO handle any number of deletions
-                _ => stream.write(ERROR_MSG).await?,
+                _ => stream.write(ERR_UNK_CMD).await?,
             };
         } else {
-            stream.write(b"Error: Could not tokenize\r\n").await?;
+            stream.write(ERR_UNK_CMD).await?;
         }
     }
 }
@@ -45,20 +44,28 @@ async fn process(mut stream: TcpStream, dict: TestRCMap<String, String>) -> io::
 //\item RENAME key newkey
 //\item KEYS regex\_pattern
 
+fn resp_bulk_format(actual_data: &String) -> String {
+    //A "$" byte followed by the number of bytes composing the string (a prefixed length), terminated by CRLF.
+    //The actual string data.
+    //A final CRLF.
+    //see https://redis.io/topics/protocol
+    format!("${}\n{}\n", actual_data.len(), actual_data)
+}
 
 async fn get_req(key: &str, dict: &TestRCMap<String, String>) -> String {
     let dict = dict.read().await;
     match (*dict).get(key) {
-        Some(val) => format!("got {}\n", val),
-        None => format!("Error, key ${}$ not found\n", key),
+        Some(val) => resp_bulk_format(val),
+        None => String::from(NIL_MSG),
     }
 }
 
 async fn set_req(key: &str, val: &str, dict: &TestRCMap<String, String>) -> String {
+    //https://redis.io/commands/SET
+    //TODO Handle NX and XX
     let mut dict = dict.write().await;
     match (*dict).insert(String::from(key), String::from(val)) {
-        Some(val) => format!("set successful, old value {}\n", val), //TODO, make correct response via Redis spec
-        None => format!("set successful with new key {}\n", key),
+        _ => format!("+OK\n"),
     }
 }
 
@@ -66,14 +73,14 @@ async fn set_req(key: &str, val: &str, dict: &TestRCMap<String, String>) -> Stri
 async fn del_req(keys: Vec<&str>, dict: &TestRCMap<String, String>) -> String {
     //https://redis.io/commands/del
     let mut dict = dict.write().await;
-    let mut count:isize = 0;
+    let mut count: isize = 0;
     for key in keys {
         match (*dict).remove(key) {
             Some(_) => count += 1,
             _ => {}
         }
     }
-    format!("{}\n", count)
+    format!(":{}\n", count)
 }
 
 fn main() -> io::Result<()> {
